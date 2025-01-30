de93cc797bb7a66dfb411420d39ca73abb7d4a00
dnk80 <58820352+dnk80@users.noreply.github.com>
2025-01-30 15:41:00 -0500

    Update spark-job-access-gcs.py

--- spark-job-access-gcs.py
+++ spark-job-access-gcs.py
@@ -4,8 +4,8 @@ import sys
 BUCKET = "test-gcs-418919"
 inputUri=f"gs://{BUCKET}/test.txt" 
 outputUri=f"gs://{BUCKET}/test_file_output"
-sc = pyspark.SparkContext
+sc = pyspark.SparkContext()
 lines = sc.textFile(inputUri)
-words = lines.flatMap(lambda line: line.split)
-wordCounts = words.map(lambda word: (word, 1)).reduceByKey(Lambda counti, count2: counti + count2) 
+words = lines.flatMap(lambda line: line.split())
+wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda count1 , count2: counti + count2) 
 wordCounts.saveAsTextFile(outputUri)
